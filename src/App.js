f6e422669aa8c610a9d49e99c50a1a3567329549
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-04-28 23:24:43 +0600

    Footer Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import Footer from './Components/Footer/Footer';
 import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div>
-
       <Navbar></Navbar>
+
+
+      <Footer></Footer>
     </div>
   );
 }
