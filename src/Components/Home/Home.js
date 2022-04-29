e857febb8c37fd7d3d51f4e790bf6a7a09b81600
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-04-29 16:47:41 +0600

    Loading page updated

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import LoadingPage from '../LoadingPage/LoadingPage';
 
 const Home = () => {
     return (
         <div>
             <h1>This is home</h1>
+            <LoadingPage></LoadingPage>
         </div>
     );
 };
---
+++ src/Components/LoadingPage/LoadingPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import loading from '../../Assets/loading.gif';
+
+const LoadingPage = () => {
+    return (
+
+
+        <div className="flex items-center justify-center w-full h-full">
+            <div className="flex justify-center items-center space-x-1 text-sm text-gray-700">
+
+                <img className='w-20' src={loading} alt="" />
+
+                <h1 className='text-2xl font-semibold'>Loading ...</h1>
+            </div>
+        </div>
+
+
+    );
+};
+
+export default LoadingPage;
