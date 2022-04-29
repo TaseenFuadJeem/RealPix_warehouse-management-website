f23a18e9cd9247e20d5a3c3c90d284d650ebd5ef
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-04-29 20:30:47 +0600

    Signup component full complete

--- src/Components/LoadingPage/LoadingPage.js
+++ src/Components/LoadingPage/LoadingPage.js
@@ -5,7 +5,7 @@ const LoadingPage = () => {
     return (
 
 
-        <div className="flex items-center justify-center w-full h-full">
+        <div className="flex items-center justify-center w-screen h-screen">
             <div className="flex justify-center items-center space-x-1 text-sm text-gray-700">
 
                 <img className='w-20' src={loading} alt="" />
--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -6,6 +6,7 @@ import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../firebase/firebase.init';
 import { toast } from 'react-toastify';
+import LoadingPage from '../LoadingPage/LoadingPage';
 
 const Signup = () => {
 
@@ -28,7 +29,7 @@ const Signup = () => {
     }
 
     if (error || googleError) {
-        toast.error(`${googleError?.message || error.message}`);
+        toast.error("Something went wrong. Please try again.");
         return;
     }
 
@@ -41,57 +42,68 @@ const Signup = () => {
     return (
         <div>
 
-            <div className="min-h-screen flex flex-col justify-center sm:py-12">
+            {
+                loading || googleLoading ?
 
-                <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
+                    <LoadingPage></LoadingPage>
 
-                    <div className="bg-white  border-2 w-full rounded-lg  shadow-2xl">
+                    :
 
-                        <img className='w-32 mx-auto mt-5' src={logo} alt="" />
-                        <h1 className="font-semibold text-center text-xl">CREATE USER</h1>
+                    <div className="min-h-screen flex flex-col justify-center sm:py-12">
 
-                        <form onSubmit={handleSignup} className="px-5 py-7">
+                        <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
 
-                            <label className="font-semibold text-sm text-gray-600 pb-1 block">User name</label>
-                            <input type="text" className="outline-none border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50" required />
+                            <div className="bg-white  border-2 w-full rounded-lg  shadow-2xl">
 
-                            <label className="font-semibold text-sm text-gray-600 pb-1 block">E-mail</label>
-                            <input onChange={(e) => setEmail(e.target.value)} type="email" className="outline-none border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50" required />
+                                <img className='w-32 mx-auto mt-5' src={logo} alt="" />
+                                <h1 className="font-semibold text-center text-xl">CREATE USER</h1>
 
-                            <label className="font-semibold text-sm text-gray-600 pb-1 block">Password</label>
-                            <input onChange={(e) => setPassword(e.target.value)} type="password" className="outline-none border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50" required />
+                                <form onSubmit={handleSignup} className="px-5 py-7">
 
-                            <input onClick={() => createUserWithEmailAndPassword(email, password)} type="submit" value="Sign Up" className="transition duration-200 bg-blue-600 hover:bg-blue-700 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block" />
+                                    <label className="font-semibold text-sm text-gray-600 pb-1 block">User name</label>
+                                    <input type="text" className="outline-none border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50" required />
 
-                        </form>
+                                    <label className="font-semibold text-sm text-gray-600 pb-1 block">E-mail</label>
+                                    <input onChange={(e) => setEmail(e.target.value)} type="email" className="outline-none border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50" required />
 
-                        <div className="px-5">
+                                    <label className="font-semibold text-sm text-gray-600 pb-1 block">Password</label>
+                                    <input onChange={(e) => setPassword(e.target.value)} type="password" className="outline-none border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50" required />
 
-                            <p className='text-center text-sm text-gray-400 mb-2'>Sign up with</p>
+                                    <input onClick={() => createUserWithEmailAndPassword(email, password)} type="submit" value="Sign Up" className="transition duration-200 bg-blue-600 hover:bg-blue-700 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block" />
 
-                            <div className="grid grid-cols-1 gap-1">
+                                </form>
+
+                                <div className="px-5">
+
+                                    <p className='text-center text-sm text-gray-400 mb-2'>Sign up with</p>
+
+                                    <div className="grid grid-cols-1 gap-1">
 
 
-                                <button onClick={() => signInWithGoogle()} type="button" className="transition duration-200 border border-gray-200 text-gray-500 w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-normal text-center flex items-center justify-center"><FcGoogle style={{ fontSize: "25px", marginRight: "5px" }} />Google</button>
+                                        <button onClick={() => signInWithGoogle()} type="button" className="transition duration-200 border border-gray-200 text-gray-500 w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-normal text-center flex items-center justify-center"><FcGoogle style={{ fontSize: "25px", marginRight: "5px" }} />Google</button>
 
 
-                            </div>
+                                    </div>
 
-                        </div>
+                                </div>
 
-                        <div className="py-5">
+                                <div className="py-5">
 
-                            <div className="grid grid-cols-1 gap-1">
+                                    <div className="grid grid-cols-1 gap-1">
 
-                                <div className="text-center  whitespace-wrap">
+                                        <div className="text-center  whitespace-wrap">
 
-                                    <Link to='/login'>
-                                        <button className="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
+                                            <Link to='/login'>
+                                                <button className="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
 
-                                            <span className="inline-block ml-1">Already have an account? Then <span className='text-blue-600 font-semibold'>Login</span></span>
+                                                    <span className="inline-block ml-1">Already have an account? Then <span className='text-blue-600 font-semibold'>Login</span></span>
 
-                                        </button>
-                                    </Link>
+                                                </button>
+                                            </Link>
+
+                                        </div>
+
+                                    </div>
 
                                 </div>
 
@@ -101,9 +113,7 @@ const Signup = () => {
 
                     </div>
 
-                </div>
-
-            </div>
+            }
 
         </div>
     );
