0e8bb03d27988fedd5f7ce1073b9747b5f345266
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-05-01 01:33:21 +0600

    Shipping section added in home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Category from '../Category/Category';
+import Shipping from '../Shipping/Shipping';
 
 const Home = () => {
     return (
         <>
             <Banner></Banner>
             <Category></Category>
+            <Shipping></Shipping>
         </>
     );
 };
