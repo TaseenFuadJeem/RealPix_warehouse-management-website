15877ef6fad8102d032637d5a46a9eee04cbed65
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-05-03 01:48:04 +0600

    Add product form reset successful

--- src/Components/AddProduct/AddProduct.js
+++ src/Components/AddProduct/AddProduct.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useForm } from "react-hook-form";
+import { toast } from 'react-toastify';
 
 const AddProduct = () => {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, reset } = useForm();
     const onSubmit = data => {
-        console.log(data);
 
         const url = `http://localhost:5000/cam`;
 
@@ -18,7 +18,11 @@ const AddProduct = () => {
             .then(res => res.json())
             .then(result => {
                 console.log(result);
-            })
+            });
+
+        reset();
+
+        toast.success("Data added successfully 😄")
 
     };
 
