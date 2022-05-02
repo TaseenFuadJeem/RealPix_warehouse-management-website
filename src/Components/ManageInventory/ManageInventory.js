a9afcbde716e93181176cbe910d504c2eade9331
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-05-02 22:30:53 +0600

    Added button in manage inventory

--- src/Components/ManageInventory/ManageInventory.js
+++ src/Components/ManageInventory/ManageInventory.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import ManageProduct from '../ManageProduct/ManageProduct';
+import { MdAddCircle } from 'react-icons/md';
 
 const ManageInventory = () => {
 
@@ -17,6 +18,12 @@ const ManageInventory = () => {
 
     return (
         <div>
+
+            <h1 className='text-center text-4xl font-semibold my-5'>Manage Inventory</h1>
+
+            <button className="relative px-4 py-2 mt-5 mx-auto focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 bg-blue-600 hover:bg-blue-700 text-gray-50 rounded-xl flex items-center gap-2 ">
+                Add Product <MdAddCircle className='text-xl' />
+            </button>
 
             <div className='grid lg:grid-cols-3 gap-8 lg:px-32 my-16'>
 
