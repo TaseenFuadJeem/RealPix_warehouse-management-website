719ad6966a97f424b091ea2d38ae230ce6aa58a3
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-05-05 17:46:26 +0600

    Added env local

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCzPhfNpO4IlC4ha2CI_p0Q9pYC-jguU-s",
-    authDomain: "camera-warehouse-9d161.firebaseapp.com",
-    projectId: "camera-warehouse-9d161",
-    storageBucket: "camera-warehouse-9d161.appspot.com",
-    messagingSenderId: "814902701806",
-    appId: "1:814902701806:web:ea78ef882cd5784fa84dff"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
